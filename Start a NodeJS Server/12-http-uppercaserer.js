6aeb26f02a055b79038d8b81ada375b7e951782f
MikeLDJ <mikeu76@gmail.com>
2016-07-02 22:27:23 +0200

    .

--- Start a NodeJS Server/12-http-uppercaserer.js
+++ Start a NodeJS Server/12-http-uppercaserer.js
@@ -7,3 +7,13 @@
 *  Your server should listen on the port provided by the first argument to   *
 *  your program.                                                             *
  *****************************************************************************/
+
+var http = require('http');
+var map = require('through2-map');
+var portNumber = process.argv[2];
+var server = http.createServer(function (request, respond) {
+    request.pipe(map(function (chunk) {
+        return chunk.toString().toUpperCase()
+    })).pipe(respond)
+});
+server.listen(portNumber);
