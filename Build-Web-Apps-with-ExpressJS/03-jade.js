21179c5b3314edd2045886464156fdddc168facf
MikeLDJ <mikeu76@gmail.com>
2016-07-03 12:32:12 +0200

    task 3 complete

---
+++ Build-Web-Apps-with-ExpressJS/03-jade.js
@@ -0,0 +1,21 @@
+// ─────────────────────────────────
+//  JADE
+//  Exercise 3 of 8
+// Create an Express.js app with a home page rendered by Jade template engine.
+// The homepage should respond to /home.
+// The view should show the current date using toDateString.
+// -------------------------------------------------------------------------------
+
+var express = require('express');
+var app = express();
+var path = require('path');
+var jade = require('jade');
+
+app.set('views', process.argv[3])
+app.set('view engine', 'jade');
+
+app.get('/home', function(req, res) {
+    res.render('index', {date: new Date().toDateString()});
+});
+
+app.listen(process.argv[2]);
