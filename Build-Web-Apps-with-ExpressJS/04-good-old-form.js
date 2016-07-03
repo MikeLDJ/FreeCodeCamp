7c1da315f4b50e9bfac37b3b88da94a3826e7cab
MikeLDJ <mikeu76@gmail.com>
2016-07-03 16:59:18 +0200

    task 4

---
+++ Build-Web-Apps-with-ExpressJS/04-good-old-form.js
@@ -0,0 +1,7 @@
+// ─────────────────────────────────
+//  GOOD OLD FORM
+//  Exercise 4 of 8
+// Write a route ('/form') that processes HTML form input
+// (<form><input name="str"/></form>) and prints backwards the str value.
+// -------------------------------------------------------------------------------
+
