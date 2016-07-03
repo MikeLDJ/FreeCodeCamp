79baaaa0c405bae0f08fe34db82ccf45416f7dd6
MikeLDJ <mikeu76@gmail.com>
2016-07-03 11:21:53 +0200

    .

---
+++ Build-Web-Apps-with-ExpressJS/01-hello-world.js
@@ -0,0 +1,9 @@
+/**************************************************************************************
+ *  HELLO WORLD! (Exercise 1 of 8)                                                    *
+ *                                                                                    *
+ *  Create an Express.js app that outputs "Hello World!" when somebody goes to /home. *
+ *                                                                                    *
+ *  The port number will be provided to you by expressworks as the first argument of  *
+ *  the application, ie. process.argv[2].                                             *
+ *************************************************************************************/
+
